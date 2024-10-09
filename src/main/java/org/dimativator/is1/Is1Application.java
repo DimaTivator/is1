9e9fcf0a7b10ac4967d9adca0838ab025576bf78
DimaTivator <andriianov164@gmail.com>
2024-10-09 10:21:17 +0300

    init

---
+++ src/main/java/org/dimativator/is1/Is1Application.java
@@ -0,0 +1,13 @@
+package org.dimativator.is1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Is1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Is1Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/dimativator/is1/Is1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dimativator.is1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Is1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
